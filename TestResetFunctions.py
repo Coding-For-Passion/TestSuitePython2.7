d07ca553c6fa28227ddaf397a2539635f70e228b
Alexander Greco <wesbob12@gmail.com>
2018-06-08 18:34:48 -0600

    Update reset function

--- TestResetFunctions.py
+++ TestResetFunctions.py
@@ -1,93 +1,91 @@
 #President Test Score Reset:
-def aPrezReset():
-	#Reset A_Points
-	myfile = open('A_Points_Prez.txt', 'w')
-	myfile.write("30")
-	myfile.close()
-
-def oPrezReset():	
-	#Reset O_Points
-	myfile = open('O_Points_Prez.txt', 'w')
+def prezReset(): 
+	
+	myfile = open('1_Prez.txt', 'w')
 	myfile.write("0")
 	myfile.close()
-
-def jPrezReset():	
-	#Reset J_Points
-	myfile = open('J_Points_Prez.txt', 'w')
-	myfile.write("5")
+	
+	myfile = open('1_Name_Prez.txt', 'w')
+	myfile.write("Nobody")
 	myfile.close()
 	
-def rPrezReset():	
-	#Reset R_Points
-	myfile = open('R_Points_Prez.txt', 'w')
-	myfile.write("18")
+	myfile = open('2_Prez.txt', 'w')
+	myfile.write("0")
 	myfile.close()
 	
-def vPrezReset():	
-	#Reset V_Points
-	myfile = open('V_Points_Prez.txt', 'w')
-	myfile.write("14")
+	myfile = open('2_Name_Prez.txt', 'w')
+	myfile.write("Nobody")
 	myfile.close()
 
-def prezReset(): 
-	aPrezReset()
-	oPrezReset()
-	jPrezReset()
-	rPrezReset()
-	vPrezReset()
+	myfile = open('3_Prez.txt', 'w')
+	myfile.write("0")
+	myfile.close()
+	
+	myfile = open('3_Name_Prez.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
+	
+	myfile = open('4_Prez.txt', 'w')
+	myfile.write("0")
+	myfile.close()
+	
+	myfile = open('4_Name_Prez.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
+	
+	myfile = open('5_Prez.txt', 'w')
+	myfile.write("0")
+	myfile.close()
+	
+	myfile = open('5_Name_Prez.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
+
+
 	
 #Fifa Test Score Reset:
-def aFifaReset():
-	#Reset A_Points_Fifa
-	myfile = open('A_Points_Fifa.txt', 'w')
+def fifaReset(): 
+	
+	myfile = open('1_Fifa.txt', 'w')
 	myfile.write("0")
 	myfile.close()
 	
-def oFifaReset():	
-	#Reset O_Points_Fifa
-	myfile = open('O_Points_Fifa.txt', 'w')
+	myfile = open('1_Name_Fifa.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
+	
+	myfile = open('2_Fifa.txt', 'w')
 	myfile.write("0")
 	myfile.close()
 	
-def jFifaReset():
-	#Reset J_Points_Fifa
-	myfile = open('J_Points_Fifa.txt', 'w')
-	myfile.write("0")
+	myfile = open('2_Name_Fifa.txt', 'w')
+	myfile.write("Nobody")
 	myfile.close()
 
-def rFifaReset():	
-	#Reset R_Points_Fifa
-	myfile = open('R_Points_Fifa.txt', 'w')
+	myfile = open('3_Fifa.txt', 'w')
 	myfile.write("0")
 	myfile.close()
 	
-def vFifaReset():	
-	#Reset V_Points_Fifa
-	myfile = open('V_Points_Fifa.txt', 'w')
+	myfile = open('3_Name_Fifa.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
+	
+	myfile = open('4_Fifa.txt', 'w')
 	myfile.write("0")
 	myfile.close()
+	
+	myfile = open('4_Name_Fifa.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
+	
+	myfile = open('5_Fifa.txt', 'w')
+	myfile.write("0")
+	myfile.close()
+	
+	myfile = open('5_Name_Fifa.txt', 'w')
+	myfile.write("Nobody")
+	myfile.close()
 
-def fifaReset():
-	aFifaReset()
-	oFifaReset()
-	jFifaReset()
-	rFifaReset()
-	vFifaReset()
-def allReset():
+def AllReset():
 	fifaReset()
-	prezReset()
-def aAllReset():
-	aFifaReset()
-	aPrezReset()
-def jAllReset():
-	jFifaReset()
-	jPrezReset()
-def oAllReset():
-	oFifaReset()
-	oPrezReset()
-def rAllReset():
-	rFifaReset()
-	rPrezReset()	
-def vAllReset():
-	vFifaReset()
-	vPrezReset()		
+	prezReset()		
