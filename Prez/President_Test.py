b49bc7da24653030c53a5f91946dbb81a173342d
Alexander Greco <wesbob12@gmail.com>
2018-06-08 18:44:59 -0600

    Update President Test

--- Prez/President_Test.py
+++ Prez/President_Test.py
@@ -6,44 +6,59 @@ os.system('clear')
 raw_input("type anything to start: ")
 os.system('clear')
 
-#Register A_Points
-myfile = open('A_Points_Prez.txt', 'r')
+# Register stored Leaderboard
+myfile = open('1_Prez.txt', 'r')
 a = myfile.read()
 myfile.close()
 
-#Register O_Points
-myfile = open('O_Points_Prez.txt', 'r')
-o = myfile.read()
+myfile = open('2_Prez.txt', 'r')
+b = myfile.read()
 myfile.close()
 
-#Register J_Points
-myfile = open('J_Points_Prez.txt', 'r')
-j = myfile.read()
+myfile = open('3_Prez.txt', 'r')
+c = myfile.read()
 myfile.close()
 
-#Register V_Points
-myfile = open('V_Points_Prez.txt', 'r')
-v = myfile.read()
+myfile = open('4_Prez.txt', 'r')
+d = myfile.read()
 myfile.close()
 
-#Register R_Points
-myfile = open('R_Points_Prez.txt', 'r')
-r = myfile.read()
+myfile = open('5_Prez.txt', 'r')
+e = myfile.read()
 myfile.close()
 
+myfile = open('1_Name_Prez.txt', 'r')
+aa = myfile.read()
+myfile.close()
+
+myfile = open('2_Name_Prez.txt', 'r')
+bb = myfile.read()
+myfile.close()
+
+myfile = open('3_Name_Prez.txt', 'r')
+cc = myfile.read()
+myfile.close()
+
+myfile = open('4_Name_Prez.txt', 'r')
+dd = myfile.read()
+myfile.close()
+
+myfile = open('5_Name_Prez.txt', 'r')
+ee = myfile.read()
+myfile.close()
 #turn string values into usable int values
-R_points = int(r)
-A_points = int(a)
-O_points = int(o)
-J_points = int(j)
-V_points = int(v)
+first = int(a)
+second = int(b)
+third = int(c)
+fourth = int(d)
+fifth = int(e)
 
-#loads the arrays for scoring
-alist = [A_points, "Alexander"]
-olist = [O_points, "Olivia"]
-vlist = [V_points, "Vince"]
-rlist = [R_points, "Rebecca"]
-jlist = [J_points, "Jonathan"]
+# loads the arrays for Leaderboard
+first_list = [aa, first]
+second_list = [bb, second]
+third_list = [cc, third]
+fourth_list = [dd, fourth]
+fifth_list = [ee, fifth]
 
 #Array that stores U.S. president's names in order
 presidents = [
@@ -107,104 +122,101 @@ while True:
 	#If the user would like to test themself
 	if menu == "Take the test":
 		name = raw_input("Please enter your name: ")
+			
+		#test setting reset
+		points = 0
+		i = 45;
+			
+		#loops through all the presidents
+		while i > 0:
+			QA = raw_input("Who is the " + str(i) + " President?")
+			if QA == presidents[i-1]:
+				print "Correct!"
+				points = points + 1
+			else:
+				print "WRONG!!!"	
+				print presidents[i-1]
+			i = i - 1
+			
+		#Scoring
+			
+		#Fraction
+		print str(points) + "/45"
+			
+		#Percent
+		percent1 = float(points)/45.0
+		percent2 = int((percent1 * 100)+0.5)
+		print str(percent2) + "%"
+			
+		#Letter grade and funny comment
+		if points >= 40:
+			print "A+"
+			print "You are a Genius, or maybe just a cheater."
+		elif points >= 35:
+			print "A"
+			print "You are a Super Star, or did you have a peek for a few?"
+		elif points >= 30:
+			print "B+"
+			print "Awesome Job! You beat the Creater!"
+		elif points >= 25:
+			print "B"
+			print "Great Job!"
+		elif points >= 20:
+			print "C+"
+			print "You did pretty well!"
+		elif points >= 15:
+			print "C"
+			print "You are starting to get the hang of this!"
+		elif points >= 10:
+			print "D+"
+			print "Slightly Smarter than the average American"
+		elif points >= 5:
+			print "D"
+			print "You are an average American, try studying who the leaders of your own country were instead of watching TV!"
+		elif points >= 0:
+			print "F"
+			print "What are you, a rock?"
+			
+		#Lets the user choose when to clear screen and go back to menu
+		raw_input("Press anything to continue: ")
+		os.system('clear')
 		
-		#Tests the individuals separately so they can have there own scores
-		
-		if name == "Olivia" or "Alexander" or "Jonathan" or "Rebecca" or "Vince":
-			while True:
-				ggez = raw_input("Are you ready? ")
-				if ggez == "yes" or "Yes":
-					break
-			
-			#test setting reset
-			points = 0
-			i = 45;
-			
-			#loops through all the presidents
-			while i > 0:
-				QA = raw_input("Who is the " + str(i) + " President?")
-				if QA == presidents[i-1]:
-					print "Correct!"
-					points = points + 1
-				else:
-					print "WRONG!!!"	
-					print presidents[i-1]
-				i = i - 1
-			
-			#Scoring
-			
-			#Fraction
-			print str(points) + "/45"
-			
-			#Percent
-			percent1 = float(points)/45.0
-			percent2 = int((percent1 * 100)+0.5)
-			print str(percent2) + "%"
-			
-			#Letter grade and funny comment
-			if points >= 40:
-				print "A+"
-				print "You are a Genius, or maybe just a cheater."
-			elif points >= 35:
-				print "A"
-				print "You are a Super Star, or did you have a peek for a few?"
-			elif points >= 30:
-				print "B+"
-				print "Awesome Job! You beat the Creater!"
-			elif points >= 25:
-				print "B"
-				print "Great Job!"
-			elif points >= 20:
-				print "C+"
-				print "You did pretty well!"
-			elif points >= 15:
-				print "C"
-				print "You are starting to get the hang of this!"
-			elif points >= 10:
-				print "D+"
-				print "Slightly Smarter than the average American"
-			elif points >= 5:
-				print "D"
-				print "You are an average American, try studying who the leaders of your own country were instead of watching TV!"
-			elif points >= 0:
-				print "F"
-				print "What are you, a rock?"
-			
-			#Lets the user choose when to clear screen and go back to menu
-			raw_input("Press anything to continue: ")
-			os.system('clear')
-			
-			#Update scores
-			if name == "Alexander":
-				if A_points < points:
-					A_points = points
-					myfile = open('A_Points.txt', 'w')
-					myfile.write(str(A_points))
-					myfile.close()
-			elif name == "Olivia":
-				if O_points < points:
-					O_points = points
-					myfile = open('O_Points.txt', 'w')
-					myfile.write(str(O_points))
-					myfile.close()
-			elif name == "Jonathan":
-				if J_points < points:
-					J_points = points	
-					myfile = open('J_Points.txt', 'w')
-					myfile.write(str(J_points))
-					myfile.close()
-			elif name == "Vince":
-				if V_points < points:
-					V_points = points
-					myfile = open('V_Points.txt', 'w')
-					myfile.write(str(V_points))
-					myfile.close()
-			elif name == "Rebecca":
-				if R_points < points:
-					R_points = points	
-					myfile = open('R_Points.txt', 'w')
-					myfile.write(str(R_points))
-					myfile.close()
+		#Update scores
+		if first < points:
+			first = points
+			myfile = open('1_Prez.txt', 'w')
+			myfile.write(str(first))
+			myfile.close()
+			first_list[0] = name
+			print "You got 1st place!"
+		elif second < points:
+			second = points
+			myfile = open('2_Prez.txt', 'w')
+			myfile.write(str(second))
+			myfile.close()
+			second_list[0] = name
+			print "You got 2nd place!"
+		elif third < points:
+			third = points
+			myfile = open('3_Prez.txt', 'w')
+			myfile.write(str(third))
+			myfile.close()
+			third_list[0] = name
+			print "You got 3rd place!"
+		elif fourth < points:
+			fourth = points
+			myfile = open('4_Prez.txt', 'w')
+			myfile.write(str(fourth))
+			myfile.close()
+			ffourth_list[0] = name
+			print "You got 4th place!"
+		elif fifth < points:
+			fifth = points
+			myfile = open('5_Prez.txt', 'w')
+			myfile.write(str(fifth))
+			myfile.close()
+			fifth_list[0] = name
+			print "You got 5th place!"
 	#Same as the regular test, but it doesn't move to the next president if you are wrong and doesn't give the right answer if you are wrong
 	#Also doesn't affect leaderboard 
 	if menu == "Repetition":
@@ -277,54 +289,68 @@ while True:
 	
 	#Shows all of the scores in order
 	if menu == "Read the leaderboard": #Set the stats
-		alist = [A_points, "Alexander"]
-		olist = [O_points, "Olivia"]
-		vlist = [V_points, "Vince"]
-		rlist = [R_points, "Rebecca"]
-		jlist = [J_points, "Jonathan"]
-		#Combine the stats
-		leaderboard = [alist,
-		olist,
-		jlist, vlist,
-		 rlist]
-		#Order the stats
-		leaderboard2 = sorted(leaderboard)
-		#Print out the stats([-*] goes backwards in the stats because it is in asending order)
-		print "1. "
-		print str(leaderboard2[-1][1]) + " -- " + strleaderboard2[-1][0])
-		print "2. "
-		print str(leaderboard2[-2][1]) + " -- " + str(leaderboard2[-2][0])
-		print "3. "
-		print str(leaderboard2[-3][1]) + " -- " + str(leaderboard2[-3][0])
-		print "4. "
-		print str(leaderboard2[-4][1]) + " -- " + str(leaderboard2[-4][0])
-		print "5. "
-		print str(leaderboard2[-5][1]) + " -- " + str(leaderboard2[-5][0])
+		first_list[1] = [first]
+		second_list[1] = [second]
+		third_list[1] = [third]
+		fourth_list[1] = [fourth]
+		fifth_list[1] = [fifth]
+
+		#Combine the Leaderboard
+		leaderboard = [first_list,
+		second_list,
+		third_list, fourth_list,
+		 fifth_list]
+
+		#Print out the Leaderboard
+		print "1. " + str(leaderboard[0][0]) + " -- " + str(leaderboard[0][1])
+		print "2. " + str(leaderboard[1][0]) + " -- " + str(leaderboard[1][1])
+		print "3. " + str(leaderboard[2][0]) + " -- " + str(leaderboard[2][1])
+		print "4. " + str(leaderboard[3][0]) + " -- " + str(leaderboard[3][1])
+		print "5. " + str(leaderboard[4][0]) + " -- " + str(leaderboard[4][1])
 		#Moves on when user is ready
 		raw_input("Press anything to continue: ")
 		os.system('clear')
 	
 	#Saves current scores and quits program
 	if menu == "Leave":
-		
-		myfile = open('A_Points_Prez.txt', 'w')
-		myfile.write(str(A_points))
+
+		myfile = open('1_Prez.txt', 'w')
+		myfile.write(str(first))
 		myfile.close()
 
-		myfile = open('O_Points_Prez.txt', 'w')
-		myfile.write(str(O_points))
+		myfile = open('2_Prez.txt', 'w')
+		myfile.write(str(second))
 		myfile.close()
 
-		myfile = open('J_Points_Prez.txt', 'w')
-		myfile.write(str(J_points))
+		myfile = open('3_Prez.txt', 'w')
+		myfile.write(str(third))
 		myfile.close()
 
-		myfile = open('V_Points_Prez.txt', 'w')
-		myfile.write(str(V_points))
+		myfile = open('4_Prez.txt', 'w')
+		myfile.write(str(fourth))
 		myfile.close()
 
-		myfile = open('R_Points_Prez.txt', 'w')
-		myfile.write(str(R_points))
+		myfile = open('5_Prez.txt', 'w')
+		myfile.write(str(fifth))
 		myfile.close()
-		
+
+		myfile = open('1_Name_Prez.txt', 'w')
+		myfile.write(str(first))
+		myfile.close()
+
+		myfile = open('2_Name_Prez.txt', 'w')
+		myfile.write(str(second))
+		myfile.close()
+
+		myfile = open('3_Name_Prez.txt', 'w')
+		myfile.write(str(third))
+		myfile.close()
+
+		myfile = open('4_Name_Prez.txt', 'w')
+		myfile.write(str(fourth))
+		myfile.close()
+
+		myfile = open('5_Name_Prez.txt', 'w')
+		myfile.write(str(fifth))
+		myfile.close()
 		break
